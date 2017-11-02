fd20b1a861fd51a5cfa3f81f55f47be8b060bf7b
Amit Varman <AmitKumarVarman@gmail.com>
2017-11-02 14:01:23 +0000

    read secrets

--- examples-go/go-echo/go-echo.go
+++ examples-go/go-echo/go-echo.go
@@ -7,23 +7,51 @@ INPUT == OUTPUT
 
 */
 import (
-	"io/ioutil"
 	"net/http"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
+
+type Message struct {
+}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 
-	//Read Post body
-	responseData, err := ioutil.ReadAll(r.Body)
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
 	if err != nil {
-		http.Error(w, err.Error(), 400)
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	secret, err := clientset.Core().Secrets("default").Get("echo", metav1.GetOptions{})
+	println(string(secret.Data["content"]))
+
+	if err != nil {
+		println("Error getting secret %s: %v", "echo", err)
 		return
 	}
-	defer r.Body.Close()
 
-	//Write Post body
+	//read the content
+	tokenContent := string(secret.Data["content"])
+
+	//Read Post body
+	// responseData, err := ioutil.ReadAll(r.Body)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), 400)
+	// 	return
+	// }
+	// defer r.Body.Close()
+
+	// write the content back in repsonse
 	w.Header().Set("content-type", "application/json")
-	w.Write(responseData)
+	w.Write([]byte(tokenContent))
 }
 
 // func main() {
